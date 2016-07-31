53ad95d38e96f3b3691159a6a728665a21e8d1fb
steelbrain <de.steelbrain@gmail.com>
2016-07-31 05:50:22 -0700

    :white_check_mark: Add specs for helpers

--- decls/atom.js
+++ decls/atom.js
@@ -7,3 +7,8 @@ declare module 'atom' {
   declare var Range: any;
   declare var TextEditor: any;
 }
+
+declare function it(name: string, callback: (() => void)): void;
+declare function fit(name: string, callback: (() => void)): void;
+declare function expect(value: any): Object;
+declare function describe(name: string, callback: (() => void)): void;
